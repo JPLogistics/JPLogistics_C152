0e69ca8f368931b6f2644f4aaf2ff971bfdbdeee
oliverpope03 <oliverpope03@gmail.com>
2021-07-05 00:03:42 +0100

    DME WORKING!!!
    DME with TIME and Distance for both NAV 1 and 2

--- Project Files/Packages/jplogistics-c152/html_ui/Pages/VCockpit/Instruments/Generic/JPLDME.js
+++ Project Files/Packages/jplogistics-c152/html_ui/Pages/VCockpit/Instruments/Generic/JPLDME.js
@@ -42,36 +42,30 @@ class JPLDME extends BaseInstrument {
 
             //# Nav 1 Script
             if (this.getIsSignalOk(1)) {
-                if (this.isLocalizer(1)) {
-                    this.nav1Error = true;
-                }
-                else {
-                    this.nav1Error = false;
-                }
+                this.nav1Error = false;
             }
             else {
                 this.nav1Error = true;
             }
 
             //# Nav 2 Script
-            /*if (this.getIsSignalOk(2)) {
-                if (this.isLocalizer(2)) {
-                    this.nav2Error = true;
-                }
-                else {
-                    this.nav2Error = false;
-                }
+            if (this.getIsSignalOk(2)) {
+                this.nav2Error = false;
             }
             else {
                 this.nav2Error = true;
-            }*/
+            }
 
             //# General
             if (this.elapsedTime < 3) {
                 diffAndSetAttribute(this.welcomeScreen, "state", "on");
                 diffAndSetAttribute(this.normalScreen, "state", "off");
                 diffAndSetText(this.welcome, "JPLogistics DME System");
-                diffAndSetText(this.welcomeLow, "Starting...");
+                this._msg = "Starting";
+                for (let i = 0; i < this.elapsedTime; i++){
+                    this._msg += ".";
+                }
+                diffAndSetText(this.welcomeLow, this._msg);
             }
             else {
                 diffAndSetAttribute(this.welcomeScreen, "state", "off");
@@ -90,8 +84,7 @@ class JPLDME extends BaseInstrument {
                     }
                     else {
                         diffAndSetText(this.nav1Distance, this.getDMEDistance(1) + " NM");
-                        //diffAndSetText(this.nav2Time, this.getDMETime(1) + " Mins");
-                        diffAndSetText(this.nav1Time, "05:00 Mins");
+                        diffAndSetText(this.nav1Time, this.getDMETime(1) + " Mins");
                     }
                     //# Nav 2 Script
                     if (this.nav2Error) {
@@ -100,8 +93,7 @@ class JPLDME extends BaseInstrument {
                     }
                     else {
                         diffAndSetText(this.nav2Distance, this.getDMEDistance(2) + " NM");
-                        //diffAndSetText(this.nav2Time, this.getDMETime(2) + " Mins");
-                        diffAndSetText(this.nav2Time, "05:00 Mins");
+                        diffAndSetText(this.nav2Time, this.getDMETime(2) + " Mins");
                     }
                 }
             }
@@ -117,7 +109,15 @@ class JPLDME extends BaseInstrument {
         }
     }
     getDMEDistance(_num) {
-        return Math.round(SimVar.GetSimVarValue("NAV DME:" + _num, "nautical mile") * 10) / 10;
+        return Math.round(SimVar.GetSimVarValue("NAV DME:" + _num, "Nautical miles") * 10) / 10;
+    }
+    getDMETime(_num) {
+        this.time = (this.getDMEDistance(_num) / SimVar.GetSimVarValue("NAV DMESPEED:" + _num, "Knots")) * 60;
+        this.minutes = Math.floor(this.time);
+        this.seconds = Math.round((this.time - this.minutes) * 60);
+        if (this.seconds < 10) { this.seconds = "0" + this.seconds; }
+        if (this.minutes > 99) { this.minutes = 99; this.seconds = 59; }
+        return (this.minutes + ":" + this.seconds);
     }
     lowBatteryCheck() {
         if (SimVar.GetSimVarValue("ELECTRICAL BATTERY VOLTAGE", "volt") > 21.6) {
@@ -143,8 +143,8 @@ class JPLDME extends BaseInstrument {
         var minutes = Math.floor(this.elapsedTime / 60000);
         return (minutes > 0 ? fastToFixed(minutes, 0) : "") + " : " + (seconds < 10 ? "0" + fastToFixed(seconds, 0) : fastToFixed(seconds, 0));
     }
-    getIsSignalOk() {
-        return Simplane.getNavHasNav();
+    getIsSignalOk(_num) {
+        return Simplane.getNavHasNav(_num);
     }
     isLocalizer(_num) {
         return SimVar.GetSimVarValue("NAV HAS LOCALIZER:" + _num, "Bool");
