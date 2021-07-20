d5bad5296e4516c00e429d930603fc6bbf616eba
oliverpope03 <oliverpope03@gmail.com>
2021-07-20 12:53:16 +0100

    Rust Setup V0.0001

---
+++ Project Files/src/Rust/C152_Systems/target/rls/debug/deps/C152_Systems-47a6e14ad6a93999.d
@@ -0,0 +1,5 @@
+c:\Users\olive\Documents\GitHub\JPLogistics_C152\Project Files\src\Rust\C152_Systems\target\rls\debug\deps\C152_Systems-47a6e14ad6a93999.rmeta: src\main.rs
+
+c:\Users\olive\Documents\GitHub\JPLogistics_C152\Project Files\src\Rust\C152_Systems\target\rls\debug\deps\C152_Systems-47a6e14ad6a93999.d: src\main.rs
+
+src\main.rs:
