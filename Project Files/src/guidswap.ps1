bd4518e0a19d88c3b218796bf244a15feefb1b3f
N95JPL <37276225+N95JPL@users.noreply.github.com>
2021-07-21 02:04:47 +0100

    Created C152 Fairings Edition / Corrected 3D Model / Corrected Animations

--- Project Files/src/guidswap.ps1
+++ Project Files/src/guidswap.ps1
@@ -23,4 +23,26 @@ $null = $sbNew.Append($text.Substring($lastStart))
 
 $sbNew.ToString() | out-file -encoding ASCII $outputFilename
 
+Write-Output "Interior Done"
+
+$filename = "C:\Users\joshu\Documents\GitHub\JPLogistics_C152\Project Files\src\JPLogistics_C152\SimObjects\Airplanes\Asobo_C152\model\C152.XML"
+$outputFilename = "C:\Users\joshu\Documents\GitHub\JPLogistics_C152\Project Files\src\JPLogistics_C152\SimObjects\Airplanes\Asobo_C152\model\C152.XML"
+
+$text = [string]::join([environment]::newline, (get-content -path $filename))
+
+$sbNew = new-object system.text.stringBuilder
+
+$pattern = "[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}"
+
+$lastStart = 0
+$null = ([regex]::matches($text, $pattern) | %{
+    $sbNew.Append($text.Substring($lastStart, $_.Index - $lastStart))
+    $guid = [system.guid]::newguid()
+    $sbNew.Append($guid)
+    $lastStart = $_.Index + $_.Length
+})
+$null = $sbNew.Append($text.Substring($lastStart))
+
+$sbNew.ToString() | out-file -encoding ASCII $outputFilename
+
 Write-Output "Done"
